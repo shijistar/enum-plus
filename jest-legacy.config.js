5cb1954dde7bd535be8071e2b9d0c68fcb528081
Leo(sage starr) <shijistar@gmail.com>
2025-05-25 22:09:09 +0800

    ci: update CI workflows to enable e2e tests

--- jest-legacy.config.js
+++ jest-legacy.config.js
@@ -1,16 +1,24 @@
 'use strict';
 
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
+const nodeVersion = process.versions.node.split('.').map(Number)[0];
+
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tslib/test/**/*.{spec,test}.js'],
-  setupFilesAfterEnv: ['<rootDir>/tslib/test/jest.setup.js'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/lib/**/*.js'],
   coveragePathIgnorePatterns: ['<rootDir>/lib/types.js'],
   moduleNameMapper: {
-    '^@enum-plus': '<rootDir>/lib',
     '^@enum-plus/(.*)$': '<rootDir>/lib/$1',
+    '^@enum-plus': '<rootDir>/lib',
   },
 };
+if (nodeVersion < 14) {
+  // @ts-expect-error: because setupTestFrameworkScriptFile is a deprecated API
+  config.setupTestFrameworkScriptFile = '<rootDir>/tslib/test/jest.setup.js';
+} else {
+  config.setupFilesAfterEnv = ['<rootDir>/tslib/test/jest.setup.js'];
+}
+
+module.exports = config;
