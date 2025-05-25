cfb25bc80ccf95fc3b2e1dfb80f2ad8fd72378c2
Leo(sage starr) <shijistar@gmail.com>
2025-05-25 20:19:11 +0800

    fix: ci scripts ttt

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,8 +1,8 @@
-import { defaultLocalize, Enum } from '../src';
-import { getLocales, setLang } from './data/week-config';
+// import { defaultLocalize, Enum } from '../src';
+// import { getLocales, setLang } from './data/week-config';
 
 beforeAll(() => {
-  setLang('en-US', Enum, getLocales, defaultLocalize);
+  // setLang('en-US', Enum, getLocales, defaultLocalize);
   jest.spyOn(console, 'warn').mockImplementation(() => {
     // Mock console.warn to suppress warnings during tests
   });
