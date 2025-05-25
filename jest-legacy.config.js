a73fed424624c04017452e00dc3e4ab20e283a3a
Leo(sage starr) <shijistar@gmail.com>
2025-05-25 19:39:59 +0800

    fix: ci scripts

--- jest-legacy.config.js
+++ jest-legacy.config.js
@@ -6,8 +6,6 @@ const nodeVersion = process.versions.node.split('.').map(Number)[0];
 const config = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tslib/test/**/*.{spec,test}.js'],
-  setupFiles: ['<rootDir>/tslib/test/jest.setup.js'],
-  setupFilesAfterEnv: ['<rootDir>/tslib/test/jest.setup.js'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/lib/**/*.js'],
   coveragePathIgnorePatterns: ['<rootDir>/lib/types.js'],
--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,4 +1,4 @@
-import { defaultLocalize, Enum } from '@enum-plus';
+import { defaultLocalize, Enum } from '../src';
 import { getLocales, setLang } from './data/week-config';
 
 beforeAll(() => {
