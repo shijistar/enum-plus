ec6bbb26decf9178e1f151e227de2ec51a439ff0
Leo(sage starr) <shijistar@gmail.com>
2025-05-26 13:46:56 +0800

    ci: update the cache strategy to improve performance

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,17 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
- *
- * This source code is licensed under the MIT license found in the LICENSE file in the root
- * directory of this source tree.
- */
-
-'use strict';
-
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/test/**/*.{spec,test}.{ts,tsx}'],
   setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
   collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coveragePathIgnorePatterns: ['src/types.ts', 'src/extension.d.ts'],
+  moduleNameMapper: {
+    '^@enum-plus/(.*)$': '<rootDir>/src/$1',
+    '^@enum-plus': '<rootDir>/src',
+  },
   coverageThreshold: {
     global: {
       branches: 100,
@@ -21,12 +19,5 @@ module.exports = {
       lines: 100,
       statements: 100,
     },
-  },
-  coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  coveragePathIgnorePatterns: ['src/types.ts', 'src/extension.d.ts'],
-  moduleNameMapper: {
-    '^@enum-plus/(.*)$': '<rootDir>/src/$1',
-    '^@enum-plus': '<rootDir>/src',
   },
 };
