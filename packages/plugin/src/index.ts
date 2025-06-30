6d4a99c32d86c0b9c785a04462d2fe6e3568464a
李凤宝(Sage Starr) <shijistar@gmail.com>
2025-06-30 23:20:09 +0800

    feat: add toSelectPlugin

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,4 +1,6 @@
 import { Enum } from 'enum-plus';
 import searchItemsPlugin from './plugins/searchItems';
+import toSelectPlugin from './plugins/toSelect';
 
 Enum.install(searchItemsPlugin);
+Enum.install(toSelectPlugin);
